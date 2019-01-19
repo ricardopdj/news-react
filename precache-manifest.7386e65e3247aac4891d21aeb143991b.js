a6540bcada76b5ac4d37c28e74f57046d79e92e3
Ricardo <pareja.r@gmail.com>
2019-01-19 16:38:50 -0200

    Updates

---
+++ precache-manifest.7386e65e3247aac4891d21aeb143991b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fbff9caa55d1977862ffe3fcf8d6343",
+    "url": "/news-react/static/media/gaucha.7fbff9ca.jpg"
+  },
+  {
+    "revision": "08cbe5e2f91495199a07",
+    "url": "/news-react/static/js/runtime~main.08cbe5e2.js"
+  },
+  {
+    "revision": "62e25e86a09c11908c31",
+    "url": "/news-react/static/js/main.62e25e86.chunk.js"
+  },
+  {
+    "revision": "e813363c048c08a0cd35",
+    "url": "/news-react/static/js/1.e813363c.chunk.js"
+  },
+  {
+    "revision": "62e25e86a09c11908c31",
+    "url": "/news-react/static/css/main.643aca82.chunk.css"
+  },
+  {
+    "revision": "e813363c048c08a0cd35",
+    "url": "/news-react/static/css/1.af00d47d.chunk.css"
+  },
+  {
+    "revision": "8f28628732f93303dea4f16ec355e7e5",
+    "url": "/news-react/index.html"
+  }
+];
